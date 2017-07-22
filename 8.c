da1ade6e2ea29fb623465612c4bd7157ec50bf2b
indhu0701 <30342676+indhu0701@users.noreply.github.com>
2017-07-22 16:28:27 +0530

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,i, sum=0;
+printf("enter a positive integer:");
+scanf("%d,&n");
+for(i=1;1<n;++i)
+{
+sum=sum+i;
+}
+sum+sum+i;
+}
+printf("sum=%d",sum);
+return 0;
+}
